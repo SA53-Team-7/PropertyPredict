7a4ef67e2258f6232fe038375921f66bce3f8d1a
frozenpotatoes <52646745+si-thu-zaw@users.noreply.github.com>
2022-01-27 17:33:39 +0800

    Initial commit

---
+++ src/main/java/com/team7/propertypredict/PropertypredictApplication.java
@@ -0,0 +1,13 @@
+package com.team7.propertypredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertypredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertypredictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team7/propertypredict/PropertypredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.propertypredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertypredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
