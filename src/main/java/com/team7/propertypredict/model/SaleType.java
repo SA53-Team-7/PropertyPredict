17601c505ec543657f74143697d1d201db6c88bb
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-02-04 01:08:58 +0800

    Update model

---
+++ src/main/java/com/team7/propertypredict/model/SaleType.java
@@ -0,0 +1,54 @@
+package com.team7.propertypredict.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="saletype")
+public class SaleType {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer typeId;
+	
+	@OneToMany(mappedBy = "saleType", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER) 
+	private List<Transaction> transactions;
+
+	public SaleType() {
+		super();
+	}
+
+	public SaleType(Integer typeId, List<Transaction> transactions) {
+		super();
+		this.typeId = typeId;
+		this.transactions = transactions;
+	}
+
+	public Integer getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(Integer typeId) {
+		this.typeId = typeId;
+	}
+
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+	
+	
+	
+
+}
