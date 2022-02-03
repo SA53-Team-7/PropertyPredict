c1228be552825342456ed58e1759de1d70b6077d
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-02-03 15:18:01 +0800

    Added ProjectController

--- src/main/java/com/team7/propertypredict/repository/ProjectRepository.java
+++ src/main/java/com/team7/propertypredict/repository/ProjectRepository.java
@@ -1,9 +1,16 @@
 package com.team7.propertypredict.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.team7.propertypredict.model.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+	
+	@Query("Select p from projects p where p.street = :street")
+	ArrayList<Project> findProjectsByStreet(@Param ("street") String street);
 
 }
