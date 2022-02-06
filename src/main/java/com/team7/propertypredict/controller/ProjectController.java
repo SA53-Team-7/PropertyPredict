c6cb5ce2344be054c694df46a1cbcf1870b7dc4d
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-02-06 12:02:21 +0800

    Update Project Controller and projects.html

--- src/main/java/com/team7/propertypredict/controller/ProjectController.java
+++ src/main/java/com/team7/propertypredict/controller/ProjectController.java
@@ -1,15 +1,11 @@
 package com.team7.propertypredict.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.team7.propertypredict.model.Project;
 import com.team7.propertypredict.model.ProjectDetails;
 import com.team7.propertypredict.service.ProjectService;
 
@@ -20,16 +16,11 @@ public class ProjectController {
 	@Autowired
 	private ProjectService pService;
 
-	// View all the projects in a given location
-	@GetMapping("/view/{street}")
-	public String viewProject(@PathVariable String street, Model model) {
-		ArrayList<Project> projects = pService.findProjectsByStreet(street);
-		ArrayList<ProjectDetails> projectDetails = new ArrayList<ProjectDetails>();
-		for(Project project:projects) {
-			ProjectDetails pd = pService.getProjectDetails(project.getProjectId());
-			projectDetails.add(pd);
-		}
-		model.addAttribute("projects", projectDetails);
-		return "projects";
+	// View project details given a project id
+	@GetMapping("/view/{pid}")
+	public String viewProject(@PathVariable Integer pid, Model model) {
+		ProjectDetails projectDetails = pService.getProjectDetails(pid);
+		model.addAttribute("project", projectDetails);
+		return "property";
 	}
 }
