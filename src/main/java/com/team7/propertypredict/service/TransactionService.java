1a4caa8b8ba0369e8869746fd5ddebf46f5dd717
Si Thu Zaw <sithuzaw36@gmail.com>
2022-02-07 11:10:42 +0800

    Removed unnecessary imports

--- src/main/java/com/team7/propertypredict/service/TransactionService.java
+++ src/main/java/com/team7/propertypredict/service/TransactionService.java
@@ -1,22 +1,15 @@
 package com.team7.propertypredict.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.team7.propertypredict.model.Project;
 import com.team7.propertypredict.model.Transaction;
-
-import helper.ProjectHelper;
 
 @Service
 public interface TransactionService {
 	
 	List<Transaction> findAllTransactions();
-
-	List<Transaction> getTransactionsByProjectId(Integer id);
 
 	// Gets all transactions for a specific project
 	List<Transaction> getTransactionsByProjectId(Integer id);
