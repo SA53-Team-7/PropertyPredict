0ca6c2f0bb1002a453e3e3cdf1cf5d62440529f0
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-02-08 21:32:15 +0800

    Added login.html and messages.properties

--- src/main/java/com/team7/propertypredict/controller/CommonController.java
+++ src/main/java/com/team7/propertypredict/controller/CommonController.java
@@ -1,5 +1,35 @@
 package com.team7.propertypredict.controller;
 
-public class CommonController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.team7.propertypredict.model.User;
+import com.team7.propertypredict.service.UserService;
+
+@Controller
+public class CommonController {
+	
+	@Autowired
+	private UserService uService;
+
+	@GetMapping("/")
+	public String index(HttpSession session) {
+
+		if (session.getAttribute("user") == null) {
+			return "redirect:/login";
+		} else {
+			return "redirect:/home";
+		}
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "login";
+	}
 }
--- src/main/java/com/team7/propertypredict/service/UserService.java
+++ src/main/java/com/team7/propertypredict/service/UserService.java
@@ -1,5 +1,8 @@
 package com.team7.propertypredict.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 }
