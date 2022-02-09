3d40ab31374a5eb76e50c5a22ceccb4f8faeb650
Ashira Wee <weeashira@gmail.com>
2022-02-09 18:53:17 +0800

    Search function

--- src/main/java/com/team7/propertypredict/controller/CommonController.java
+++ src/main/java/com/team7/propertypredict/controller/CommonController.java
@@ -1,5 +1,7 @@
 package com.team7.propertypredict.controller;
 
+import java.util.ArrayList;
+
 // import java.util.ArrayList;
 
 // import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,38 +29,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.team7.propertypredict.model.Project;
 import com.team7.propertypredict.model.User;
+import com.team7.propertypredict.service.ProjectService;
+import com.team7.propertypredict.service.TransactionService;
 import com.team7.propertypredict.service.UserService;
 
 @Controller
 public class CommonController {
 	
-// 	@Autowired
-// 	TransactionService tService;
+ 	@Autowired
+ 	TransactionService tService;
 	
-// 	@Autowired
-// 	ProjectService pService;
+ 	@Autowired
+ 	ProjectService pService;
 	
-// 	@GetMapping("/")
-// 	public String viewHome(Model model) {
-// 		model.addAttribute("districtFilter", tService.getDistinctDistrict());
-// 		model.addAttribute("propTypeFilter", tService.getDistinctPropertyType());
-// 		model.addAttribute("segmentFilter", pService.findDistinctSegment());
-// 		return "index";
-// 	}
-	
-// 	@RequestMapping(value = "/search", method = RequestMethod.GET) 
-// 	public String submitSearchRequest(Model model, @Param("searchStr") String searchStr, @Param("district") String district, 
-// 			@Param("propertyType") String propertyType, @Param("segment") String segment) {
-		
-// 		String districtModified = district.compareTo("All") == 0 ? "" : district;
-// 		String typeModified = propertyType.compareTo("All") == 0 ? "" : propertyType;
-// 		String segmentModified = segment.compareTo("All") == 0 ? "" : segment;
-		
-// 		ArrayList<Project> result = pService.searchProjectsWeb(searchStr, segmentModified, districtModified, typeModified);
-// 		model.addAttribute("searchresult", result);
-// 		return "search-result";
-
 	private UserService uService;
 
 	@GetMapping("/")
@@ -109,4 +95,26 @@ public class CommonController {
 		session.invalidate();
 		return "redirect:/";
 	}
+	
+	
+ 	@GetMapping("/home-temp")
+ 	public String viewHome(Model model) {
+ 		model.addAttribute("districtFilter", tService.getDistinctDistrict());
+ 		model.addAttribute("propTypeFilter", tService.getDistinctPropertyType());
+ 		model.addAttribute("segmentFilter", pService.findDistinctSegment());
+ 		return "index";
+ 	}
+	
+ 	@RequestMapping(value = "/search", method = RequestMethod.GET) 
+ 	public String submitSearchRequest(Model model, @Param("searchStr") String searchStr, @Param("district") String district, 
+ 			@Param("propertyType") String propertyType, @Param("segment") String segment) {
+		
+ 		String districtModified = district.compareTo("All") == 0 ? "" : district;
+ 		String typeModified = propertyType.compareTo("All") == 0 ? "" : propertyType;
+ 		String segmentModified = segment.compareTo("All") == 0 ? "" : segment;
+		
+ 		ArrayList<Project> result = pService.searchProjectsWeb(searchStr, segmentModified, districtModified, typeModified);
+ 		model.addAttribute("searchresult", result);
+ 		return "search-result";
+ 	}
 }
