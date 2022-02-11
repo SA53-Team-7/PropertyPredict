df0224577d7d14105f9b5bc36f6a5f5a5bd758a9
Lina <82659876+Wanderstarz@users.noreply.github.com>
2022-02-11 16:47:24 +0800

    Added OneMap REST API to get amenity details through OneMap search

--- src/main/java/com/team7/propertypredict/controller/MapRestController.java
+++ src/main/java/com/team7/propertypredict/controller/MapRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.team7.propertypredict.helper.AmenityAPI;
 import com.team7.propertypredict.helper.Map;
 
 @RestController
@@ -18,6 +19,16 @@ public class MapRestController {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		Map result = restTemplate.getForObject(url, Map.class);
+		return result;
+	}
+	
+	@GetMapping(value="/amenity-details")
+	public AmenityAPI getAmenityDetails(String search) {
+		
+		String url = "https://developers.onemap.sg/commonapi/search?searchVal=" + search + "&returnGeom=Y&getAddrDetails=N";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		AmenityAPI result = restTemplate.getForObject(url, AmenityAPI.class);
 		return result;
 	}
 }
---
+++ src/main/java/com/team7/propertypredict/helper/AmenityAPI.java
@@ -0,0 +1,51 @@
+package com.team7.propertypredict.helper;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AmenityAPI {
+	 
+	private Integer found;
+	private Integer totalNumPages;
+	private Integer pageNum;
+	private Results results;
+	
+	public AmenityAPI() {
+		super();
+	}
+
+	public Integer getFound() {
+		return found;
+	}
+
+	public void setFound(Integer found) {
+		this.found = found;
+	}
+
+	public Integer getTotalNumPages() {
+		return totalNumPages;
+	}
+
+	public void setTotalNumPages(Integer totalNumPages) {
+		this.totalNumPages = totalNumPages;
+	}
+
+	public Integer getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(Integer pageNum) {
+		this.pageNum = pageNum;
+	}
+
+	public Results getResults() {
+		return results;
+	}
+
+	public void setResults(Results results) {
+		this.results = results;
+	}
+	
+	
+
+}
