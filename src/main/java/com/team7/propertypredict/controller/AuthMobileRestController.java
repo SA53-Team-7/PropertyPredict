da6afbea519ceada77ee96db6c4eee8d3855afd4
Oliver Tan <61538053+ohanyang@users.noreply.github.com>
2022-02-17 18:22:28 +0800

    authentication rest API for mobile

---
+++ src/main/java/com/team7/propertypredict/controller/AuthMobileRestController.java
@@ -0,0 +1,63 @@
+package com.team7.propertypredict.controller;
+
+import com.team7.propertypredict.model.User;
+import com.team7.propertypredict.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/mobile/auth")
+public class AuthMobileRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public Map<String, Integer> loginFromMobile (@RequestBody User user) {
+        Map<String, Integer> success = new HashMap<>();
+
+        if (userService.authenticate(user.getEmail(), user.getPassword()) != null) {
+            success.put("login", 1);
+            return success;
+        }
+        else {
+            success.put("login", 0);
+            return success;
+        }
+    }
+
+    @PostMapping("/register")
+    public Map<String, Integer> registerFromMobile (@RequestBody User newUser) {
+        Map<String, Integer> success = new HashMap<>();
+
+        if (userService.findUserByEmail(newUser.getEmail()) == null) {
+            userService.save(newUser);
+            success.put("register", 1);
+            return success;
+        }
+        else {
+            success.put("register", 0);
+            return success;
+        }
+    }
+
+    @PostMapping("/name")
+    public Map<String, String> getUserFirstName (@RequestBody User user) {
+        Map<String, String> success = new HashMap<>();
+
+        User result = userService.findUserByEmail(user.getEmail());
+
+        if (result.getUsername() == null) {
+            success.put("name", user.getEmail());
+            return success;
+        }
+        else {
+            success.put("name", result.getUsername());
+            return success;
+        }
+    }
+
+}
